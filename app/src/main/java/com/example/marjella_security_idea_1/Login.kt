2e1a66e0ad8a08a8a1a661936c46d8fed2f98c24
PGK16POWERS <136324051+PGK16POWERS@users.noreply.github.com>
2025-04-24 12:55:02 +0200

    KT

--- app/src/main/java/com/example/marjella_security_idea_1/Login.kt
+++ app/src/main/java/com/example/marjella_security_idea_1/Login.kt
@@ -5,15 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.navigation.fragment.findNavController
 
 class Login : Fragment() {
+
+    private lateinit var createAccountRedirect : LinearLayout
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+
+        createAccountRedirect = view.findViewById(R.id.redirect_to_sign_up);
+
+        createAccountRedirect.setOnClickListener { _ -> {
+            findNavController().navigate(R.id.SIgn_up)
+        } }
+
+        return view
     }
 
 }
---
+++ app/src/main/java/com/example/marjella_security_idea_1/SIgn_up.kt
@@ -0,0 +1,21 @@
+package com.example.marjella_security_idea_1
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class SIgn_up : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_s_ign_up, container, false)
+
+        return view
+    }
+
+}
