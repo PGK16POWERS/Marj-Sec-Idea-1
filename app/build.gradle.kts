09ab2041ce1856f852258a2162ca24e8566c4a76
PGK16POWERS <136324051+PGK16POWERS@users.noreply.github.com>
2025-04-25 08:16:25 +0200

    MISC

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,8 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.core.splashscreen)
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
