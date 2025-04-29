261ddd25e5311812eb4dbdc500c3950fa6059261
PGK16POWERS <136324051+PGK16POWERS@users.noreply.github.com>
2025-04-29 22:51:28 +0200

    KT

--- app/src/main/java/com/example/marjella_security_idea_1/network/apiService.kt
+++ app/src/main/java/com/example/marjella_security_idea_1/network/apiService.kt
@@ -4,9 +4,11 @@ import com.example.marjella_security_idea_1.models.Login
 import com.example.marjella_security_idea_1.models.LoginResponse
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface ApiService {
     @POST("/android/auth/login")
     suspend fun login(@Body login: Login): Response<LoginResponse>
+
 }
